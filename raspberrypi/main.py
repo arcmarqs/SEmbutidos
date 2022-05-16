639cfdec43aea1406a0558bf9b72279fe25a1962
sofiamalpique <sofiamalpique@gmail.com>
2022-05-16 20:49:43 +0100

    feat: Update Python API to return JSON

--- raspberrypi/main.py
+++ raspberrypi/main.py
@@ -1,88 +1,113 @@
+
 import os
 import logging
 from random import randint
 import flask
 import warnings
 
-
-warnings.filterwarnings("ignore", category=FutureWarning)
-
-
 # Set up logging
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S')
 
-
 logging.info('Initialising app')
 app = flask.Flask(__name__)
 
-# Inicialização do dia de trabalho
-num_pessoas = 0
-num_atendidos = 0
+# State
 
-# A VARIÁVEL SEGUINTE SERÁ APAGADA!! ESSA INFO DEVE VIR DO ARDUINO
-num_guiche = 3
+currentNumber = 0
+highestNumber = 0
+guicheArray = [
+    {
+        "name": "Guiche 1",
+        "number": None,
+        "pin": None
+    }, {
+        "name": "Guiche 2",
+        "number": None,
+        "pin": None
+    }
+]
 
-def gerar_pin():
-    codigo = randint(0,9999)
-    codigo_em_str = str(codigo)
-    zero_filled_number = codigo_em_str.zfill(4)
-    return zero_filled_number
+
+# Helper functions
+
+def generatePin():
+    return str(randint(0, 9999)).zfill(4)
 
 
-# End-point implementation.
+@app.route('/getState')
+def getState():
+    return flask.jsonify({
+        "currentNumber": currentNumber,
+        "highestNumber": highestNumber,
+        "guicheArray": guicheArray
+    })
 
-@app.route('/')
-def index():
-    return flask.render_template('index.html')
 
-@app.route('/aumentar_num_senhas')
-def aumentar_num_senhas():
-    global num_pessoas
-    num_pessoas += 1
-    data = num_pessoas
-    print("xxxxx")
-    print(num_pessoas)
-    print(data)
-    return flask.render_template('aumentar_num_senhas.html', data=data)
+@app.route('/getCurrentNumber')
+def getCurrentNumber():
+    return flask.jsonify({
+        "currentNumber": currentNumber
+    })
 
-@app.route('/aumentar_num_atendidos')
-def aumentar_num_atendidos():
-    global num_atendidos
-    num_atendidos += 1
-    data = num_atendidos
-    print("xxxxx")
-    print(num_atendidos)
-    print(data)
-    return flask.render_template('aumentar_num_atendidos.html', data=data)
+@app.route('/getGuicheArray')
+def getGuicheArray():
+    return flask.jsonify(guicheArray)
 
-@app.route('/ver_meu_pin')
-def ver_meu_pin():
-    data = gerar_pin()
-    return flask.render_template('ver_meu_pin.html', data=data)
-
-@app.route('/esta_quase')
-def esta_quase():
-    global num_atendidos
-    # isto deve ser aplicado ao meu nº de senha ao invés da "última senha" que é
-    # num_pessoas
-    global num_pessoas
-    if num_pessoas == num_atendidos + 1:
-        data = "A seguir sou eu!"
+@app.route('/checkTicket')
+def checkTicket():
+    global currentNumber
+    global highestNumber
+    if currentNumber == highestNumber -1:
+        return flask.jsonify(True)
     else:
-        data = "Ainda falta..."
-    return flask.render_template('/esta_quase.html', data=data)
+        return flask.jsonify(False)
 
-@app.route('/arduino_mandou_id')
-def arduino_mandou_id():
-    cod = gerar_pin()
-    global num_guiche
-    global num_pessoas
-    tupulo = ("{}".format(num_pessoas),"{}".format(num_guiche),"{}".format(cod))
-    data = tupulo
+@app.route('/getTicket')
+def getTicket():
+    global guicheArray
+    global currentNumber
+    global highestNumber
+
+    highestNumber = highestNumber + 1;
+
+    for guiche in guicheArray:
+        if (guiche["number"] == None):
+            currentNumber = highestNumber
+            guiche["number"] = highestNumber
+            guiche["pin"] = generatePin()
+            break
+
+    return flask.jsonify({
+        "highestNumber": highestNumber
+    })
+
+@app.route('/checkIn')
+def checkIn():
+    global guicheArray
+    global currentNumber
+    global highestNumber
     
-    return flask.render_template('/arduino_mandou_id.html', data=data)
+    guicheNumber = flask.request.args.get('guicheNumber')
+    pinCode = flask.request.args.get('pinCode')
+
+    wasCheckedIn = False
+
+    for guiche in guicheArray:
+        if (str(guiche["number"]) == guicheNumber and guiche["pin"] == pinCode):
+            wasCheckedIn = True
+            guiche["number"] = None
+            guiche["pin"] = None
+            if(highestNumber > currentNumber):
+                currentNumber = currentNumber + 1
+                guiche["number"] = currentNumber
+                guiche["pin"] = generatePin()
+            break
+
+    return flask.jsonify({
+        "wasCheckedIn": wasCheckedIn
+    })
 
 
 if __name__ == '__main__':
